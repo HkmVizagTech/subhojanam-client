4ede7bcd8474df90d7b3ee8503494b03c20336d8
sai kiran <saikiran@sais-MacBook-Air.local>
2026-03-26 15:37:30 +0530

    the new video updared

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -9,8 +9,13 @@ function VideoSection() {
 
       <div className="video-box">
         <iframe
-         src="https://youtu.be/cJoNqu4YqTk?si=ujuCjEL7uGwHgj_s"        
-          title="Subhojanam Video"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/cJoNqu4YqTk?si=ujuCjEL7uGwHgj_s"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
         ></iframe>
       </div>
